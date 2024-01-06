053b0ec4804c44543e9c0c4c5db3aaa09a593318
Evan Chan <evanchan040511@gmail.com>
2024-01-06 23:41:20 +0800

    open in new window

--- argo_functions.py
+++ argo_functions.py
@@ -5,7 +5,7 @@ import collections.abc
 from pptx import Presentation
 
 def makelink(a, bfp, endpoint):
-    return f"<a href='{endpoint+bfp}#page={a}'>'{a}'</a>"
+    return f"<a target='scherzo' href='{endpoint+bfp}#page={a}'>'{a}'</a>"
 
 def search_pptx_files(word, output_stream, http_mode=False, endpoint=""):
     current_dir = os.getcwd()
@@ -29,9 +29,9 @@ def search_pptx_files(word, output_stream, http_mode=False, endpoint=""):
         if http_mode:
             bfp2 = os.path.basename(file_path).replace(".pptx",".pdf")
             if found_pages:
-                output_stream.write(f"Word '{word}' found in <a href='{endpoint+bfp2}'>'{bfp2}'</a>, on page(s): {','.join(makelink(a, bfp2, endpoint) for a in map(str, found_pages))}<br>")
+                output_stream.write(f"Word '{word}' found in <a target='scherzo' href='{endpoint+bfp2}'>'{bfp2}'</a>, on page(s): {','.join(makelink(a, bfp2, endpoint) for a in map(str, found_pages))}<br>")
             else:
-                output_stream.write(f"Word '{word}' not found in <a href='{endpoint+bfp2}'>'{bfp2}'</a>.<br>")
+                output_stream.write(f"Word '{word}' not found in <a target='scherzo' href='{endpoint+bfp2}'>'{bfp2}'</a>.<br>")
         else:
             if found_pages:
                 output_stream.write(f"Word '{word}' found in '{os.path.basename(file_path)}', on page(s): {','.join(map(str, found_pages))}\n")
