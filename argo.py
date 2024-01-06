dcd7dc3639496b24ba37f50811e92bee0c611b3c
Evan Chan <evanchan040511@gmail.com>
2024-01-06 14:23:50 +0800

    Use LinkStart since will be eventually useful for some people...

--- argo.py
+++ argo.py
@@ -1,3 +1,34 @@
+import_string = \
+'''
+from pptx import Presentation # python-pptx
+'''
+
+### LINK START! (https://github.com/evnchn/linkstart.py)
+for line in import_string.splitlines():
+    if "import" in line:
+        print(line)
+        try:
+            exec(line)
+        except:
+            if "#" in line:
+                package_name = line.split("#")[-1]
+            else:
+                splits = line.split("import")
+                if "from" in line:
+                    package_name = splits[0].replace("from","")
+                else:
+                    package_name = splits[1]
+            package_name = package_name.strip()
+            print("Installing {}...".format(package_name))    
+            import subprocess
+            import sys
+            subprocess.check_call([sys.executable, "-m", "pip", "install", package_name])
+            try:
+                exec(line)
+            except:
+                print("Failed to install {}".format(package_name))
+### DONE
+
 import os
 import glob
 import collections
